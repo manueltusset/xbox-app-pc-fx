7f3e5c00c55c081c89de7e9922e2e83166273547
manueltusset <tussetmanuel@gmail.com>
2021-02-14 21:10:28 -0300

    Initialize repo

---
+++ src/com/tusset/xbox/FXMLDocumentController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tusset.xbox;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+
+/**
+ *
+ * @author manue
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
---
+++ src/com/tusset/xbox/Main.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tusset.xbox;
+
+import java.net.URL;
+import javafx.application.Application;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import netscape.javascript.JSObject;
+
+/**
+ *
+ * @author manue
+ */
+public class Main extends Application {
+    
+    private JSObject javascriptConnector;
+    
+    private JavaConnector javaConnector = new JavaConnector();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        WebView webView = new WebView();
+        URL url = Main.class.getResource("page/index.html");
+        final WebEngine webEngine = webView.getEngine();
+        
+        webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (Worker.State.SUCCEEDED == newValue) {
+                JSObject window = (JSObject) webEngine.executeScript("window");
+                window.setMember("javaConnector", javaConnector);
+                javascriptConnector = (JSObject) webEngine.executeScript("getJsConnector()");
+            }
+        });
+        
+        Scene scene = new Scene(webView);
+        
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setMaximized(true);
+        stage.setResizable(false);
+        stage.setWidth(1920);
+        stage.setHeight(1080);
+        stage.show();
+        
+        webEngine.load(url.toString());
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    
+    public class JavaConnector {
+        public void toLowerCase(String value) {
+            javascriptConnector.call("showResult", value.toLowerCase());
+        }
+    }
+}
